0a2a106e35646aeaba60f3f97fcdbff926b25d19
Hitesh Rupani <rupanihiteshabc@gmail.com>
2024-10-13 20:24:44 +0530

    Initial Commit

---
+++ SkinCancerDetection/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SkinCancerDetection
+//
+//  Created by Hitesh Rupani on 13/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SkinCancerDetection/SkinCancerDetectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkinCancerDetectionApp.swift
+//  SkinCancerDetection
+//
+//  Created by Hitesh Rupani on 13/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SkinCancerDetectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
