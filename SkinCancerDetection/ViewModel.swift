0922908362d6db1b5f4187f4a6abf812ae73e5c8
Hitesh Rupani <rupanihiteshabc@gmail.com>
2024-10-19 01:04:01 +0530

    added UI to start detecting

---
+++ SkinCancerDetection/ViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  ViewModel.swift
+//  SkinCancerDetection
+//
+//  Created by Hitesh Rupani on 13/10/24.
+//
+
+import Foundation
+import UIKit
+import PhotosUI
+
+class ViewModel: ObservableObject {
+    
+    @Published var image: UIImage?
+    
+    
+}
